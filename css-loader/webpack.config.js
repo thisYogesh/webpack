84ae1de50be509fa3485e3a9fef816973d71abb6
Yogesh <yog99mail@gmail.com>
2019-04-21 20:33:16 +0530

    added css loader example

--- css-loader/webpack.config.js
+++ css-loader/webpack.config.js
@@ -6,5 +6,15 @@ module.exports = {
     output: {
         filename: 'build.js',
         path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
     }
 }
